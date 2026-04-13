03a01b9dc74a4af685a1c8c26646ef0c3d96b683
Arosh Athukorala <aroshmathukorala@gmail.com>
2026-04-13 15:45:46 +0530

    added reducer

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,4 +1,6 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { CartReducer } from "../reducer/CartReducer";
+import { type } from "@testing-library/user-event/dist/type";
 
 const initialState = {
   cartList: [],
@@ -8,8 +10,35 @@ const initialState = {
 const CardContext = createContext(initialState);
 
 export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(CartReducer, initialState);
+
+  const addToCart = (product) => {
+    const updatedCartList = state.cartList.concat(product);
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        products: updatedCartList,
+      },
+    });
+  };
+
+  const removeFromCart = (product) => {
+    const updatedCartList = state.cartList.filter(
+      (current) => current.id !== product.id,
+    );
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: {
+        product: updatedCartList,
+      },
+    });
+  };
+
   const value = {
-    total: 3,
+    total: state.total,
+    cartList: state.cartList,
+    addToCart,
+    removeFromCart,
   };
 
   return <CardContext.Provider value={value}>{children}</CardContext.Provider>;
---
+++ src/reducer/CartReducer.js
@@ -0,0 +1,12 @@
+export const CartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "ADD_TO_CART":
+      return { ...state, cartList: payload.products };
+    case "REMOVE_FROM_CART":
+      return { ...state, cartList: payload.products };
+    default:
+      throw new Error("No case found in CartReducer");
+  }
+};
